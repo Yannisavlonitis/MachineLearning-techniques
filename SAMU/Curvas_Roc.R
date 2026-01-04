1cd2a7ee5d3a954507355a61e5160aa12565d85a
samuelpintosgonzalez3a <samuelpintosgonzalez3a@gmail.com>
2026-01-04 19:37:20 +0100

    Update Curvas_Roc.R

--- SAMU/Curvas_Roc.R
+++ SAMU/Curvas_Roc.R
@@ -1,51 +1,55 @@
-# 1. PREPARACIÓN PREVIA (Asegurar formato)
-# Es vital que la Clase sea factor para que los modelos y las curvas funcionen
+# 1. PREPARACIÓN Y LIBRERÍAS
+library(pROC)
+library(PRROC)
+library(mda)      
+library(gbm)      
+library(e1071)    
+
 df_final$Clase <- as.factor(df_final$Clase)
 
-# 2. CÁLCULO DE AUC Y CURVAS ROC
-# Nota: Asumo que testData es una partición de df_final
-# Sustituimos 'primaryormetastasis' por 'Clase'
-
-# Lista de modelos y sus probabilidades para automatizar un poco
-modelos_list <- list(
-  knn = probabilities_knn[,2],
-  svm_lin = probabilities_svm_linear[,2],
-  svm_ker = probabilities_svm_kernel[,2],
-  svm_pol = probabilities_svm_kernelpol[,2],
-  dt = probabilities_dt[,2],
-  lda = probabilities_lda$posterior[,2],
-  rda = probabilities_rda$posterior[,2]
+# 2. LISTA DE PROBABILIDADES
+# Corrección 1: Aseguramos que 'nb' y otros extraigan la columna correcta (prob. de la clase positiva)
+# Nota: Verifica si tu clase positiva es la 1 o la 2 en levels(testData$Clase)
+modelos_probs <- list(
+  knn      = probabilities_knn[,2],
+  svm_lin  = probabilities_svm_linear[,2],
+  svm_rad  = probabilities_svm_kernel[,2],
+  svm_pol  = probabilities_svm_kernelpol[,2],
+  dt       = probabilities_dt[,2],
+  lda      = probabilities_lda$posterior[,2],
+  qda      = probabilities_qda$posterior[,2],
+  rda      = probabilities_rda$posterior[,2],
+  # Corrección 2: predict de mda (FDA) devuelve matriz; necesitamos la columna de la clase positiva
+  fda      = predict(model_fda, testData, type = "posterior")[,2],
+  nb       = probabilities_nb[,2],
+  boosting = predict(model_gbm, testData, n.trees = 100, type = "response")
 )
 
-# Calcular ROCs
-roc_knn      <- roc(testData$Clase, modelos_list$knn)
-roc_svm_lin  <- roc(testData$Clase, modelos_list$svm_lin)
-roc_svm_ker  <- roc(testData$Clase, modelos_list$svm_ker)
-roc_svm_pol  <- roc(testData$Clase, modelos_list$svm_pol)
-roc_dt       <- roc(testData$Clase, modelos_list$dt)
-roc_lda      <- roc(testData$Clase, modelos_list$lda)
+# 3. CÁLCULO DE CURVAS ROC
+# Corrección 3: En pROC, a veces hay que especificar los niveles para evitar confusión de dirección
+calcular_roc <- function(prob, labels) {
+  valid <- !is.na(prob)
+  # Usamos levels(labels) para que pROC sepa exactamente el orden de las clases
+  roc(labels[valid], prob[valid], quiet = TRUE, levels = levels(labels))
+}
 
-# Manejo de NA para RDA (común en datasets genómicos con alta colinealidad)
-valid_rda    <- !is.na(probabilities_rda$posterior[, 2])
-roc_rda      <- roc(testData$Clase[valid_rda], modelos_list$rda[valid_rda])
+rocs <- lapply(modelos_probs, calcular_roc, labels = testData$Clase)
 
-# --- VISUALIZACIÓN ROC ---
-plot(roc_knn, col = "blue", main = "Curvas ROC: Clasificación de Tumores", lwd = 2)
-plot(roc_svm_lin, col = "red", add = TRUE, lwd = 2)
-plot(roc_svm_ker, col = "green", add = TRUE, lwd = 2)
-plot(roc_svm_pol, col = "orange", add = TRUE, lwd = 2)
-plot(roc_dt, col = "purple", add = TRUE, lwd = 2)
-plot(roc_lda, col = "pink", add = TRUE, lwd = 2)
-plot(roc_rda, col = "yellow", add = TRUE, lwd = 2)
+# 4. VISUALIZACIÓN ROC UNIFICADA
+colores <- c("blue", "red", "darkgreen", "orange", "purple", "deeppink", "gold", "brown", "cyan", "darkmagenta", "darkgray")
 
-legend("bottomright", 
-       legend = c(paste("k-NN:", round(auc(roc_knn), 2)),
-                  paste("SVM Lin:", round(auc(roc_svm_lin), 2)),
-                  paste("SVM Ker:", round(auc(roc_svm_ker), 2)),
-                  paste("SVM Pol:", round(auc(roc_svm_pol), 2)),
-                  paste("Tree:", round(auc(roc_dt), 2)),
-                  paste("LDA:", round(auc(roc_lda), 2)),
-                  paste("RDA:", round(auc(roc_rda), 2))),
-       col = c("blue", "red", "green", "orange", "purple", "pink", "yellow"), 
-       lwd = 2, cex = 0.8)
+# Aseguramos que el gráfico se limpie antes de dibujar
+plot.new() 
+plot(rocs[[1]], col = colores[1], main = "Comparativa ROC: Diagnóstico Genético", lwd = 2)
 
+for(i in 2:length(rocs)) {
+  # Usamos la función genérica lines() o plot(..., add=TRUE)
+  plot(rocs[[i]], col = colores[i], add = TRUE, lwd = 2)
+}
+
+# Leyenda con AUC redondeado
+legend_labels <- sapply(names(rocs), function(n) {
+  paste(toupper(n), ":", round(as.numeric(auc(rocs[[n]])), 2))
+})
+
+legend("bottomright", legend = legend_labels, col = colores, lwd = 2, cex = 0.6, ncol = 2, bty = "n")
